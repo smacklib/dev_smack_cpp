751e1274f4315b04912665dcc0f9d70e2662d750
Michael Binz <michael.binz@daimler.com>
2021-03-25 17:15:00 +0100

    Added test implementation for argcv support.

---
+++ test/cli_class_argcv.cpp
@@ -0,0 +1,110 @@
+/*
+ * Smack C++
+ *
+ * Copyright © 2019 Michael Binz
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+#include "test_common.hpp"
+
+#include "../src/smack_cli.hpp"
+
+using std::cout;
+using std::endl;
+
+static int free_function(int p1) {
+    std::cout <<
+        __func__ <<
+        " : " <<
+        p1 <<
+        std::endl;
+    return EXIT_SUCCESS;
+}
+
+class TestApplication
+{
+    std::string name_;
+
+public:
+    TestApplication(std::string name) :
+        name_(name) {
+    }
+
+    // Ensures that we do not accidentally copy the host application.
+    TestApplication(const TestApplication&) = delete;
+    TestApplication& operator=(const TestApplication&) = delete;
+
+    int f1(int p1) {
+        return smack::test::common::f(__func__, p1);
+    }
+
+    int f2(int p1, const char* p2) {
+        return smack::test::common::f(__func__, p1, p2);
+    }
+
+    int f3(int p1, double p2, const char* p3) {
+        return smack::test::common::f(__func__, p1, p2, p3);
+    }
+
+    int f4(const std::string p1) {
+        return smack::test::common::f(__func__, p1);
+    }
+
+    int f5(const std::string& p1) {
+        return smack::test::common::f(__func__, p1);
+    }
+
+    int f6(bool p1) {
+        return smack::test::common::f(__func__, p1);
+    }
+
+    int f7(int p1) const {
+        return smack::test::common::f(__func__, p1);
+    }
+    int f7_2(int p1, double p2) const {
+        return smack::test::common::f(__func__, p1, p2);
+    }
+
+    int execute(int argc, char **argv) {
+        using smack::cli::Commands;
+
+        auto cli = smack::cli::makeCliApplication(
+            Commands::make<&TestApplication::f1>(
+                "eins", this),
+            Commands::make<&TestApplication::f2>(
+                "zwei", this),
+            Commands::make<&TestApplication::f3>(
+                "drei", this),
+            Commands::make<&TestApplication::f4>(
+                "vier", this),
+            Commands::make<&TestApplication::f5>(
+                "fuenf", this),
+            Commands::make<&TestApplication::f6>(
+                "sechs", this),
+            Commands::make<&TestApplication::f7>(
+                "sieben", this),
+            Commands::make<&TestApplication::f7_2>(
+                "sieben_2", this ),
+            Commands::make<free_function>(
+                "acht") );
+
+        return cli.launch(argc,argv);
+    }
+};
+
+int main(int argc, char**argv)
+{
+    TestApplication ta{ 
+        argv[0] 
+    };
+
+    std::vector<std::string> cmdArgv(
+        argv + 1, 
+        argv + argc);
+
+    return ta.execute(argc, argv);
+}
---
+++ test/cli_functional_argcv.cpp
@@ -0,0 +1,96 @@
+/*
+ * Smack C++
+ *
+ * Copyright © 2019 Michael Binz
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "test_common.hpp"
+
+#include "../src/smack_cli.hpp"
+
+namespace {
+
+using smack::cli::Commands;
+
+int f1(int p1) {
+    return smack::test::common::f(__func__, p1);
+}
+
+int f2(int p1, const char* p2) {
+    return smack::test::common::f(__func__, p1, p2);
+}
+
+int f3(int p1, double p2, const char* p3) {
+    return smack::test::common::f(__func__, p1, p2, p3);
+}
+
+int f4(const std::string p1) {
+    return smack::test::common::f(__func__, p1);
+}
+
+int f5(const std::string& p1) {
+    return smack::test::common::f(__func__, p1);
+}
+
+int f6(bool p1) {
+    return smack::test::common::f(__func__, p1);
+}
+
+int f7(int p1) {
+    return smack::test::common::f(__func__, p1);
+}
+int f7(int p1, double p2) {
+    return smack::test::common::f(__func__, p1, p2);
+}
+
+} // namespace anonymous
+
+int main(int argc, char**argv) {
+    std::cout << argv[0] << std::endl;
+
+    auto cmd1 = Commands::make<f1>(
+        "eins",
+        { "p1" });
+
+    auto cmd2 = Commands::make<f2>(
+        "zwei");
+
+    auto cmd3 = Commands::make<f3>(
+        "drei");
+
+    auto cmd4 = Commands::make<f4>(
+        "vier");
+
+    auto cmd5 = Commands::make<f5>(
+        "fuenf");
+
+    auto cmd6 = Commands::make<f6>(
+        "sechs");
+
+    // Example for an overloaded function.
+    auto cmd7 = Commands::make<static_cast<int(*)(int)>(f7)>(
+        "sieben");
+
+    // Example for an overloaded function. Generally this is doable but
+    // does not make a lot of sense.  The alternative, a different name for
+    // the overloaded function, is much simpler.
+    auto cmd7_2 = Commands::make<static_cast<int(*)(int, double)>(f7)>(
+        "sieben_2");
+
+    auto cli = smack::cli::makeCliApplication(
+        cmd1,
+        cmd2,
+        cmd3,
+        cmd4,
+        cmd5,
+        cmd6,
+        cmd7,
+        cmd7_2
+    );
+
+    return cli.launch(argc, argv);
+}
