24b52ab5e8339eec748af9b75dc7d0e07182077e
Michael Binz <michab66@gmail.com>
2021-09-26 20:18:33 +0200

    Aligned combsort to Wikipedia.

--- examples/cli_functional_2.cpp
+++ examples/cli_functional_2.cpp
@@ -2,7 +2,7 @@
 #include <utility>
 #include <cstdlib>
 
-// std::swap constexpr in c++20
+// std::swap is constexpr in c++20
 template<class T>
 constexpr void swap(T& l, T& r)
 {
@@ -10,37 +10,6 @@ constexpr void swap(T& l, T& r)
     l = std::move(r);
     r = std::move(tmp);
 }
-
-template <typename T, size_t N>
-struct cx_array
-{
-    constexpr T& operator[](size_t i)
-    {
-        return arr[i];
-    }
-
-    constexpr const T& operator[](size_t i) const
-    {
-        return arr[i];
-    }
-
-    constexpr const T* begin() const
-    {
-        return arr;
-    }
-    constexpr const T* end() const
-    {
-        return arr + N;
-    }
-
-    using size_type = size_t;
-
-    constexpr size_type size() const {
-        return N;
-    }
-
-    T arr[N];
-};
 
 template <typename T, size_t N>
 using A = std::array<T,N>;
@@ -64,22 +33,28 @@ constexpr void quick_sort_impl(A<T, N> &to_sort, size_t left, size_t right)
 }
 
 template <typename T, size_t N>
-constexpr void comb_sort_impl(A<T, N> &to_sort, size_t left, size_t right)
+constexpr void comb_sort_impl_2(A<T, N> &to_sort, size_t left, size_t right)
 {
-    using size_type = typename cx_array<T,N>::size_type;
-    size_type gap = to_sort.size ( );
-    bool swapped = false;
-    while ( ( gap > size_type { 1 } ) or swapped ) {
-        if ( gap > size_type { 1 } ) {
-            gap = static_cast<size_type> ( gap / 1.247330950103979 );
+    using S = typename A<T,N>::size_type;
+
+    auto gap = to_sort.size();
+    auto shrink = 1.3;
+    bool sorted = false;
+    
+    while ( ! sorted ) 
+    {
+        gap = static_cast<S> ( gap / shrink );
+
+        if ( gap <= 1 ) {
+            gap = 1;
+            sorted = true;
         }
-        swapped = false;
-        for ( size_type i = size_type { 0 }; gap + i < static_cast<size_type> ( to_sort.size() ); ++i ) {
+
+        for ( S i = 0 ; i + gap < to_sort.size() ; ++i )
+        { 
             if ( to_sort[ i ] > to_sort[ i + gap ] ) {
-                auto swap = to_sort[ i ];
-                to_sort[ i ] = to_sort[ i + gap ];
-                to_sort[ i + gap ] = swap;
-                swapped = true;
+                swap( to_sort[ i ], to_sort[ i + gap ] );
+                sorted = false;
             }
         }
     }
@@ -89,11 +64,11 @@ template <typename T, size_t N>
 constexpr A<T, N> sort(A<T, N> cx_array)
 {
     auto sorted = cx_array;
-    comb_sort_impl(sorted, 0, N);
+    comb_sort_impl_2(sorted, 0, N);
     return sorted;
 }
 
-constexpr A unsorted{5,7,3,4,313,1,8,2,9,0,6,10};
+constexpr A unsorted{5,7,3,4,313,1,8,2,9,0,6,10,-121};
 constexpr auto sorted = sort(unsorted);
 
 #include <iostream>
