f04d8e6942df15ac90e7420d000b6826e32c102e
Michael Binz <michab66@gmail.com>
2021-09-26 19:39:12 +0200

    Added combsort, switched to std::array.

--- examples/cli_functional_2.cpp
+++ examples/cli_functional_2.cpp
@@ -2,6 +2,7 @@
 #include <utility>
 #include <cstdlib>
 
+// std::swap constexpr in c++20
 template<class T>
 constexpr void swap(T& l, T& r)
 {
@@ -32,42 +33,52 @@ struct cx_array
         return arr + N;
     }
 
+    using size_type = size_t;
+
+    constexpr size_type size() const {
+        return N;
+    }
+
     T arr[N];
 };
 
 template <typename T, size_t N>
-constexpr void quick_sort_impl(cx_array<T, N> &cx_array, size_t left, size_t right)
+using A = std::array<T,N>;
+
+template <typename T, size_t N>
+constexpr void quick_sort_impl(A<T, N> &to_sort, size_t left, size_t right)
 {
     if (left < right)
     {
         size_t m = left;
 
         for (size_t i = left + 1; i<right; i++)
-            if (cx_array[i]<cx_array[left])
-                swap(cx_array[++m], cx_array[i]);
+            if (to_sort[i]<to_sort[left])
+                swap(to_sort[++m], to_sort[i]);
 
-        swap(cx_array[left], cx_array[m]);
+        swap(to_sort[left], to_sort[m]);
 
-        quick_sort_impl(cx_array, left, m);
-        quick_sort_impl(cx_array, m + 1, right);
+        quick_sort_impl(to_sort, left, m);
+        quick_sort_impl(to_sort, m + 1, right);
     }
 }
 
-template<typename cx_array>
-constexpr void comb_sort_impl ( cx_array & cx_array_ ) noexcept {
-    using size_type = typename cx_array::size_type;
-    size_type gap = cx_array_.size ( );
+template <typename T, size_t N>
+constexpr void comb_sort_impl(A<T, N> &to_sort, size_t left, size_t right)
+{
+    using size_type = typename cx_array<T,N>::size_type;
+    size_type gap = to_sort.size ( );
     bool swapped = false;
     while ( ( gap > size_type { 1 } ) or swapped ) {
         if ( gap > size_type { 1 } ) {
             gap = static_cast<size_type> ( gap / 1.247330950103979 );
         }
         swapped = false;
-        for ( size_type i = size_type { 0 }; gap + i < static_cast<size_type> ( cx_array_.size ( ) ); ++i ) {
-            if ( cx_array_ [ i ] > cx_array_ [ i + gap ] ) {
-                auto swap = cx_array_ [ i ];
-                cx_array_ [ i ] = cx_array_ [ i + gap ];
-                cx_array_ [ i + gap ] = swap;
+        for ( size_type i = size_type { 0 }; gap + i < static_cast<size_type> ( to_sort.size() ); ++i ) {
+            if ( to_sort[ i ] > to_sort[ i + gap ] ) {
+                auto swap = to_sort[ i ];
+                to_sort[ i ] = to_sort[ i + gap ];
+                to_sort[ i + gap ] = swap;
                 swapped = true;
             }
         }
@@ -75,14 +86,14 @@ constexpr void comb_sort_impl ( cx_array & cx_array_ ) noexcept {
 }
 
 template <typename T, size_t N>
-constexpr cx_array<T, N> sort(cx_array<T, N> cx_array)
+constexpr A<T, N> sort(A<T, N> cx_array)
 {
     auto sorted = cx_array;
-    quick_sort_impl(sorted, 0, N);
+    comb_sort_impl(sorted, 0, N);
     return sorted;
 }
 
-constexpr cx_array<int, 11> unsorted{5,7,3,4,1,8,2,9,0,6,10}; // odd number of elements
+constexpr A unsorted{5,7,3,4,313,1,8,2,9,0,6,10};
 constexpr auto sorted = sort(unsorted);
 
 #include <iostream>
